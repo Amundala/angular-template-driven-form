e3f805885e079556ed8e94e2487def241200276f
Amundala <tom.amundala@a2k.africa>
2024-05-08 09:44:07 +0300

    creating the form with NgModel two-way data binding, and json pipe for data checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroFormComponent } from './components/hero-form/hero-form.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroFormComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'template-driven-form';
--- src/app/components/hero-form/hero-form.component.ts
+++ src/app/components/hero-form/hero-form.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Hero } from '../../interfaces/heroClass';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-form',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule, CommonModule],
   templateUrl: './hero-form.component.html',
-  styleUrl: './hero-form.component.css'
+  styleUrl: './hero-form.component.css',
 })
 export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
+  //--- class instance ----
+  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+  }
 }
